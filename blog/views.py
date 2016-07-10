6bd6d50f4c3524ec303dfc77aecf45a7f9ffa96f
zhouyc <zhouyc@knownsec.com>
2016-07-11 00:35:59 +0800

    add tag functional

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponseRedirect
 from django.core.mail import send_mail
 
+from taggit.models import Tag
+
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
@@ -48,8 +50,13 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -59,7 +66,8 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts,
-                                                   'page': page})
+                                                   'page': page,
+                                                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
