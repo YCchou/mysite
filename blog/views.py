d2faa29d0583ebfc964a765a1ea0b57f169af98c
zhouyc <zhouyc@knownsec.com>
2016-07-12 00:16:42 +0800

    similar blog

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponseRedirect
 from django.core.mail import send_mail
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -36,7 +37,7 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
-                                                    'sent': sent})
+                                                    'sent': sent, })
 
 
 def home(request):
@@ -89,7 +90,14 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(
+        id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by(
+        '-same_tags', '-publish')[:4]
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
                                                      'comment_form':
-                                                     comment_form})
+                                                     comment_form,
+                                                     'similar_posts':similar_posts})
